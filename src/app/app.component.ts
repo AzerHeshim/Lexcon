a45b42c00555faf56a5e8ae601f0fe557be062a2
unknown <azer.hashimov@knexel.com>
2021-09-25 18:07:51 +0400

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 declare var $: any;
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Lexcon';
-  moreOpened: boolean = false
+  moreOpened: boolean = false;
   openMore(): void{
    this.moreOpened = !this.moreOpened;
   }
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
+
+  ngOnInit(): void {
+    AOS.init();
+  }
+
 }
 $(document).on('scroll', function () {
   if ($(document).scrollTop() > 200) {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
