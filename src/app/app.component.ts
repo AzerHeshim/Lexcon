6131e367ef3335b00e82f3139b0faa9e7a93cb16
unknown <azer.hashimov@knexel.com>
2021-10-04 20:05:59 +0400

    m

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,10 @@ export class AppComponent implements OnInit{
   title = 'Lexcon';
   moreOpened: boolean = false;
   selected = ''
+  spinning = false;
   checked = false;
   checkedName = false;
+  language = '';
   languageAzSelected = false;
   languageRuSelected = false;
   languageEnSelected = false;
@@ -24,28 +26,35 @@ export class AppComponent implements OnInit{
   //   phone: new FormControl(''),
   //   email: new FormControl(''),
   // });
+  ifSuccess = false;
+  ifError = false;
   openMore(): void{
    this.moreOpened = !this.moreOpened;
-  }
-  constructor(private translate: TranslateService) {
-    translate.setDefaultLang('en');
   }
   useLanguage(language: string): void {
     this.translate.use(language);
     if (language === 'az'){
+      localStorage.setItem('lang', language);
+      this.language = localStorage.getItem('lang')
       this.languageAzSelected = true;
       this.languageEnSelected = false;
       this.languageRuSelected = false;
     } else if (language === 'en'){
+      localStorage.setItem('lang', language);
+      this.language = localStorage.getItem('lang')
       this.languageEnSelected = true;
       this.languageRuSelected = false;
       this.languageAzSelected = false;
     } else if (language === 'ru'){
+      localStorage.setItem('lang', language);
+      this.language = localStorage.getItem('lang')
       this.languageRuSelected = true;
       this.languageAzSelected = false;
       this.languageEnSelected = false;
     }}
-
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang(localStorage.getItem('lang'));
+  }
   ngOnInit(): void {
     AOS.init();
   }
@@ -63,11 +72,20 @@ export class AppComponent implements OnInit{
   }
   onSubmit(e: Event): void{
     e.preventDefault();
+    this.spinning = true;
     emailjs.sendForm('service_xekms0g', 'template_uda672q', e.target as HTMLFormElement,  'user_Y1fCbzgmDQDRNUAs0ssy7')
       .then((result: EmailJSResponseStatus) => {
         console.log(result.text);
+        // this.mailForm.reset();
+        this.ifSuccess = true;
+        this.ifError = false;
+        this.spinning = false;
       }, (error) => {
         console.log(error.text);
+        // this.mailForm.reset()
+        this.ifSuccess = false;
+        this.ifError = true;
+        this.spinning = false;
       });
     // console.warn(this.mailForm.value);
   }
